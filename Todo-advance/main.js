a16a7b4a387352be9e627a7eb1ceddc427abdf19
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-15 15:14:48 +0600

    form validation done

--- Todo-advance/main.js
+++ Todo-advance/main.js
@@ -37,46 +37,78 @@ currentMonth.innerHTML = month;
 // ===============> start todo functionality <================
 const addTaskBtn = document.querySelector('.add_button');
 const todoBody = document.querySelector('.todo_body');
+const form = document.getElementById('form');
+const formSubmitBtn = document.getElementById('submit_btn');
+const formCloseBtn = document.getElementById('close_btn');
+const inputTitle = document.getElementById('input_title');
+const inputText = document.getElementById('input_text');
+const noticeMsg = document.getElementById('notice_msg');
 
-//------> todo-item div create
-const todoItem = document.createElement('div');
-todoItem.className = 'todo_item';
-todoBody.appendChild(todoItem);
 
-//------> todo-title create
-const todoTitleBox_h4 = document.createElement('h4');
-todoItem.appendChild(todoTitleBox_h4);
-//------> todo-icon create
-const todoIcon = document.createElement('i');
-todoIcon.className = 'fa-solid fa-angles-right point_icon';
-todoTitleBox_h4.appendChild(todoIcon);
+// ======> form submit & validation <=====
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    formValidation();
+});
 
-//-----> todo-title create
-const todo_title = document.createElement('span');
-todo_title.className = 'todo_title';
-todo_title.innerText = 'i am title';
-todoTitleBox_h4.appendChild(todo_title);
+const formValidation = () => {
+    formSubmitBtn.addEventListener('click', () => {
+        if (inputTitle.value === '') {
+            noticeMsg.innerHTML = "Your Value is Empty";
+        } else {
+            // console.log('yes, value coming');
+            noticeMsg.innerHTML = "";
+        }
+    })
+}
 
-// ------> todo-text create
-const todoText = document.createElement('p');
-todoText.innerText = 'this is text .write your task. edghfe wser waerfo ioawrf';
-todoText.className = 'todo_text';
-todoItem.appendChild(todoText);
 
-//------> edit-delete icon
-const editUpdateBox = document.createElement('div');
-editUpdateBox.className = 'edit_update_icon_box';
-todoItem.appendChild(editUpdateBox);
+// ======> create function <=====
+const createItemFun = () => {
+    //------> todo-item div create
+    const todoItem = document.createElement('div');
+    todoItem.className = 'todo_item';
+    todoBody.appendChild(todoItem);
 
-// ------> edit icon
-const editIcon = document.createElement('i');
-editIcon.className = 'fa-solid fa-pen-to-square';
+    //------> todo-title create
+    const todoTitleBox_h4 = document.createElement('h4');
+    todoItem.appendChild(todoTitleBox_h4);
+    //------> todo-icon create
+    const todoIcon = document.createElement('i');
+    todoIcon.className = 'fa-solid fa-angles-right point_icon';
+    todoTitleBox_h4.appendChild(todoIcon);
 
-const deleteIcon = document.createElement('i');
-deleteIcon.className = 'fa-solid fa-trash-can';
+    //-----> todo-title create
+    const todo_title = document.createElement('span');
+    todo_title.className = 'todo_title';
+    todo_title.innerText = 'i am title';
+    todoTitleBox_h4.appendChild(todo_title);
 
-editUpdateBox.appendChild(editIcon);
-editUpdateBox.appendChild(deleteIcon);
+    // ------> todo-text create
+    const todoText = document.createElement('p');
+    todoText.innerText = 'this is text .write your task. edghfe wser waerfo ioawrf';
+    todoText.className = 'todo_text';
+    todoItem.appendChild(todoText);
+
+    //------> edit-delete icon
+    const editUpdateBox = document.createElement('div');
+    editUpdateBox.className = 'edit_update_icon_box';
+    todoItem.appendChild(editUpdateBox);
+
+    // ------> edit icon
+    const editIcon = document.createElement('i');
+    editIcon.className = 'fa-solid fa-pen-to-square';
+
+    const deleteIcon = document.createElement('i');
+    deleteIcon.className = 'fa-solid fa-trash-can';
+
+    editUpdateBox.appendChild(editIcon);
+    editUpdateBox.appendChild(deleteIcon);
+
+
+}
+
+
 
 
 
