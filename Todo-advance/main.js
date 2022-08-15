ba4dd65c206ee982a50d67e232cd5c00d1460d40
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-15 12:05:39 +0600

    todo list box created with dom

--- Todo-advance/main.js
+++ Todo-advance/main.js
@@ -34,3 +34,35 @@ if (date == '1') {
 
 currentMonth.innerHTML = month;
 
+// ===============> start todo functionality <================
+const addTaskBtn = document.querySelector('.add_button');
+const todoBody = document.querySelector('.todo_body');
+
+//------> todo-item div create
+const todoItem = document.createElement('div');
+todoItem.className = 'todo_item';
+todoBody.appendChild(todoItem);
+
+//------> todo-title create
+const todoTitleBox_h4 = document.createElement('h4');
+todoItem.appendChild(todoTitleBox_h4);
+//------> todo-icon create
+const todoIcon = document.createElement('i');
+todoIcon.className = 'fa-solid fa-angles-right point_icon';
+todoTitleBox_h4.appendChild(todoIcon);
+
+//-----> todo-title create
+const todo_title = document.createElement('span');
+todo_title.className = 'todo_title';
+todo_title.innerText = 'i am title';
+todoTitleBox_h4.appendChild(todo_title);
+
+// ------> todo-text create
+const todoText = document.createElement('p');
+todoText.innerText = 'this is text .write your task. edghfe wser waerfo ioawrf';
+todoText.className = 'todo_text';
+todoItem.appendChild(todoText);
+
+
+
+
