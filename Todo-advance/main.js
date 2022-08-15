bbfd1544c2916b42cb9318815fa30d75d7a70f3b
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-15 11:18:51 +0600

    date time update

--- Todo-advance/main.js
+++ Todo-advance/main.js
@@ -1,2 +1,26 @@
+//==============> date time setup <=====================
+const currentDay = document.querySelector('.current_day');
 const currentDate = document.querySelector('.current_date');
 const currentMonth = document.querySelector('.current_month');
+
+
+
+//-----> current day
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const dayIndex = new Date().getDay();
+const day = days[dayIndex];
+
+// ------> current date
+const date = new Date().getDate();
+
+
+// ------> current month
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const monthIndex = new Date().getMonth();
+const month = months[monthIndex];
+
+// =============> set value
+currentDay.innerHTML = `${day},`;
+currentDate.innerHTML = `${day}th`;
+currentMonth.innerHTML = month;
+
