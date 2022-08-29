f589f7a23c692b402ba61e9377395adb44a72bef
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-08-30 00:17:22 +0600

    dynamic team add with his dynamic value

--- hami-sportsDB/js/main.js
+++ hami-sportsDB/js/main.js
@@ -41,9 +41,41 @@ const showAllLeague = (countries) => {
 // -----> show all team list
 
 const leagueTeamList = async (country) => {
-    console.log(country)
     const res = await fetch(`https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?s=Soccer&c=${country}`)
     const data = await res.json()
-        .then(data => console.log(data.teams))
+        .then(data => showTeams(data.teams))
         .catch((err) => { console.log(err) })
+}
+
+const teamContainer = document.querySelector('.team_container');
+const teamInfoContainer = document.querySelector('.team_info_container');
+const teamWrapper = document.querySelector('.team_wrapper');
+const teamsArea = document.querySelector('.teams_area');
+
+// -----> show all teams
+const showTeams = (teamsData) => {
+    // console.log(teamsData)
+    leagueArea.innerHTML = "";
+    teamsArea.classList.remove('hidden');
+    // ---> team box create
+    teamsData.forEach(team => {
+        console.log(team)
+
+        const teamBoxArea = document.createElement('div');
+        teamBoxArea.classList.add('team_box_area');
+        teamBoxArea.innerHTML = `
+                         <div class="team_box p-6 bg-slate-800 rounded transition duration-300 hover:drop-shadow-xl  ">
+                                <img src="${team.strTeamBadge}" alt="">
+                                <h2 class="text-2xl text-rose-600 font-semibold mb-2"><span>Team : </span>${team.strTeam}</h2>
+                                <p class="text-white">${team.strDescriptionEN.slice(0, 200)}...</p>
+                                <button
+                                    class="mt-3 bg-rose-300 py-2 px-3 rounded font-medium tracking-wide transition duration-300 hover:bg-rose-600 hover:text-white"
+                                    >
+                                    Team Details
+                                </button>
+                            </div>
+        `;
+        teamWrapper.appendChild(teamBoxArea);
+    })
+
 }
