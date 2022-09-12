078728061492b57ad3096a1280fb3e0307244b44
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-09-12 23:38:50 +0600

    countries info founder

--- home/js/data.js
+++ home/js/data.js
@@ -6,5 +6,12 @@ const projectData = [
         live: '../../Banking-system/index.html',
         CodeLink: 'https://github.com/mdhamimulhaque/javascript-projects/tree/master/Banking-system',
     },
+    {
+        title: "Country's information",
+        Technology: "TailWind Css || JS DOM || API",
+        description: "Search by country name.This is a API project",
+        live: '../../country-info-founder/index.html',
+        CodeLink: 'https://github.com/mdhamimulhaque/javascript-projects/tree/master/country-info-founder',
+    },
 
 ]
