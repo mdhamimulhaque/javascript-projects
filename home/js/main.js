0477c3038243ef647ede5f96301f4f1f05a471a7
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-09-13 00:19:49 +0600

    update projects info all

--- home/js/main.js
+++ home/js/main.js
@@ -1,13 +1,13 @@
 const projectWrapper = document.querySelector('.projects_wrapper');
-
+document.getElementById('project_Total').innerText = projectData.length;
 projectData.forEach(projectInfo => {
     const { title, Technology, description, live, CodeLink } = projectInfo;
     const projectBox = document.createElement('div');
     projectBox.classList.add('project_box');
     projectBox.innerHTML = `
-<div class="card w-96 bg-base-100 shadow-xl">
+<div class="card w-96 bg-base-100 shadow-xl bg-blue-100">
 <div class="card-body">
-    <h2 class="card-title">${title}</h2>
+    <h2 class="card-title text-blue-500">${title}</h2>
     <p>${Technology}</p>
     <p>${description}</p>
     <div class="card-actions justify-end">
