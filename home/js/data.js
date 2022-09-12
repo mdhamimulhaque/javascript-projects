091c8072d5f9e89b6363a655f9ebb00cb7583adb
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-09-12 23:16:25 +0600

    hamim's bank data update

--- home/js/data.js
+++ home/js/data.js
@@ -1,10 +1,10 @@
 const projectData = [
     {
-        title: "lorem ipsum",
-        style: "css || Bootstrap",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
-        live: 'https://daisyui.com/components/card/',
-        CodeLink: 'https://www.youtube.com/watch?v=UL0gM5uB7gk',
+        title: "Hamim's Bank",
+        Technology: "TailWind Css || JS DOM",
+        description: "Banking System(withdraw, deposit system)",
+        live: '../../Banking-system/index.html',
+        CodeLink: 'https://github.com/mdhamimulhaque/javascript-projects/tree/master/Banking-system',
     },
 
 ]
--- home/js/main.js
+++ home/js/main.js
@@ -1,23 +1,23 @@
 const projectWrapper = document.querySelector('.projects_wrapper');
 
 projectData.forEach(projectInfo => {
-    const { title, style, description, live, CodeLink } = projectInfo;
+    const { title, Technology, description, live, CodeLink } = projectInfo;
     const projectBox = document.createElement('div');
     projectBox.classList.add('project_box');
     projectBox.innerHTML = `
 <div class="card w-96 bg-base-100 shadow-xl">
 <div class="card-body">
     <h2 class="card-title">${title}</h2>
-    <p>${style}</p>
+    <p>${Technology}</p>
     <p>${description}</p>
     <div class="card-actions justify-end">
         <div class="badge badge-outline">
-        <a href=${live}>
+        <a target='_blank' href=${live}>
         LIve
         </a>
         </div>
         <div class="badge badge-outline">
-        <a href=${CodeLink}>
+        <a target='_blank' href=${CodeLink}>
         Code
         </a>
         </div>
